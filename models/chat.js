fd17e558b66e1d479ce3b0fef515a921893c8aa4
Michelle Felix <mich.feloz@gmail.com>
2018-10-30 20:54:19 -0600

    Modelo y rutas chat

---
+++ models/chat.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var Usuario = require('../models/usuario');
+// var uniqueValidator = require('mongoose-unique-validator');
+
+var Schema = mongoose.Schema;
+
+// Tipos de mensajes válidos
+var tiposValidos = {
+  values: ["texto", "audio", "imagen"],
+  message: "{VALUE} no es un estatus válido"
+};
+
+var chatSchema = new Schema({
+  usuario: { type: Schema.Types.Mixed, required: [true, 'El usuario que envía el mensaje es requerido'] },
+    tipo: { type: String, required: [true, 'El tipo de chat es requerido'], enum:tiposValidos },
+    proyectoId: { type: Schema.Types.ObjectId, ref:'Proyecto',required: true },
+    fecha: { type: Date, required: [true, 'La fecha es requerida'] },
+    mensaje: { type: String, required: false },
+    audio: { type: String, required: false },
+    img: { type: String, required: false }
+
+});
+
+module.exports = mongoose.model('Chat', chatSchema);
---
+++ routes/chat.js
@@ -0,0 +1,99 @@
+var express = require('express');
+var mdAutenticacion = require('../middlewares/autenticacion');
+
+var app = express();
+
+var Chat = require('../models/chat');
+
+app.get('/:idProyecto', mdAutenticacion.verificarToken, (req, res) => {
+    var proyectoId=req.params.idProyecto;
+    var chatsCargados= req.query.chatsCargados || 0;
+
+    chatsCargados = Number(chatsCargados);
+
+        Chat.find({ 'proyectoId': proyectoId })
+                .sort('-fecha')
+                .limit(5)
+                .skip(chatsCargados)
+                .exec( (err,chats)=>{
+
+                    if(err){
+                        return res.status(400).json({
+                            ok:false,
+                            mensaje: 'Error al consultar chats del proyecto: '+ proyectoId,
+                            errors: err
+                        });
+                    }
+
+                    if(!chats){
+                        return res.status(200).json({
+                            ok:true,
+                            mensaje:'No hay chats en el proyecto: ' + proyectoId,
+                        });
+                    }
+
+                    Chat.count({ 'proyectoId': proyectoId }, (err, conteoChats) => {
+                        if (err) {
+                            return res.status(500).json({
+                                ok: false,
+                                mensaje: 'Error al contar los chats del proyecto: ' + proyectoId,
+                                errors: err
+                            });
+                        }
+
+                        res.status(200).json({
+                            ok: true,
+                            mensaje: 'Consulta de chats realizada correctamente',
+                            chats: chats,
+                            totalChats: conteoChats
+                        });
+                    
+                });
+
+        
+
+
+    });
+
+});
+
+app.post('/', mdAutenticacion.verificarToken, (req,res)=>{
+    var body = req.body;
+
+    var chat = new Chat({
+
+        usuario:body.usuario ,
+        tipo:body.tipo ,
+        proyectoId:body.proyectoId ,
+        fecha:body.fecha ,
+        mensaje:body.mensaje ,
+        audio:body.audio ,
+        img:body.img 
+
+    });
+
+    chat.save( (err, chatGuardado)=>{
+
+        if(err){
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al guardar chat',
+                errors: err
+            });
+        }
+
+        res.status(201).json({
+            ok: true,
+            mensaje: 'Mensaje guardado correctamente',
+            chat: chatGuardado
+        });
+    });
+
+
+});
+
+   
+
+
+
+module.exports = app;
