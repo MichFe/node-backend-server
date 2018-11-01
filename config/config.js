07fdd1e1f1dd8804ff2b961a479df322b9da161f
Michelle Felix <mich.feloz@gmail.com>
2018-11-01 14:38:28 -0600

    Config prod

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports.SEED = '@Mobla-Core-Software@MFP1-7';
 module.exports.CORS_OPTIONS = {
-  origin: ["https://moblacore.com","http://localhost:4200"],
+  origin: ["https://moblacore.com"],
   optionsSuccessStatus: 200
 };
