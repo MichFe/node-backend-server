0897922745bf91783876f058bbdebeb7a0ea4603
Michelle Felix <mich.feloz@gmail.com>
2018-11-02 15:41:45 -0600

    Config

--- config/config.js
+++ config/config.js
@@ -4,10 +4,10 @@ module.exports.SEED = '@Mobla-Core-Software@MFP1-7';
 // Production config
 //-------------------
 
-// module.exports.CORS_OPTIONS = {
-//   origin: ["https://moblacore.com"],
-//   optionsSuccessStatus: 200
-// };
+module.exports.CORS_OPTIONS = {
+  origin: ["https://moblacore.com"],
+  optionsSuccessStatus: 200
+};
 
 // module.exports.UPLOADS_PATH =
 //   "/Users/PascualMichelle/Desktop/Consultoria CHR/Mobla/CoreSoftware/src/assets/images/uploads/";
@@ -21,10 +21,10 @@ module.exports.SEED = '@Mobla-Core-Software@MFP1-7';
 // Development Config
 //------------------------
 
-module.exports.CORS_OPTIONS = {
-  origin: ["http://localhost:4200"],
-  optionsSuccessStatus: 200
-};
+// module.exports.CORS_OPTIONS = {
+//   origin: ["http://localhost:4200"],
+//   optionsSuccessStatus: 200
+// };
 
 // module.exports.UPLOADS_PATH =
 //   "/Users/PascualMichelle/Desktop/Consultoria CHR/Mobla/CoreSoftware/src/assets/images/uploads/";
