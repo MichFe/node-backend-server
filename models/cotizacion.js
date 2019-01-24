2377f4e9859b44efffb6cdbc6d023f35573f9626
Michelle Felix <mich.feloz@gmail.com>
2019-01-24 11:26:35 -0600

    Se cambia propiedad carrito por productos en modelo

--- models/cotizacion.js
+++ models/cotizacion.js
@@ -8,7 +8,7 @@ var cotizacionSchema = new Schema({
     proyecto: { type: Schema.Types.ObjectId, ref: 'Proyecto', required: [true, 'El id del proyecto es requerido'] },
     cliente: { type: Schema.Types.ObjectId, ref: 'Cliente', required: [true, 'El id del cliente es requerido'] },
     fecha: { type: Date, required: [true, 'La fecha de la cotizacion es requerida'] },
-    carrito: [{}],
+    productos: [{}],
     subtotal: { type: Number, required: [true, 'El subtotal es requerido'] },
     descuento: { type: Number, required: [true, 'El descuento es requerido. Si es cero, favor de indicarlo'] },
     total: { type: Number, required: [true, 'El total es requerido'] },
