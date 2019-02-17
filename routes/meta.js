4927fe561274a2ad5d761a087bb69c7dad45130d
Michelle Felix <mich.feloz@gmail.com>
2019-02-17 17:50:44 -0600

    CRUD para metas

---
+++ routes/meta.js
@@ -0,0 +1,113 @@
+var express = require('express');
+var mdAutenticacion = require('../middlewares/autenticacion');
+
+var app = express();
+
+var Meta = require('../models/meta');
+
+app.get('/:year', mdAutenticacion.verificarToken ,(req, res, next) => {
+    var year=req.params.year;
+
+    Meta.find({'year': year}, (err, metasYear)=>{
+
+        if(err){
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar metas',
+                errors:err
+            });
+        }
+
+        if(!metasYear[0]){
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'No hay metas para el año: ' + year,
+                errors: { message: 'No hay metas en la base de datos para el año especificado' }
+            });
+        }
+
+        res.status(200).json({
+            ok: true,
+            mensaje: 'Consulta de metas exitosa',
+            metas: metasYear
+        });
+
+    });
+    
+    
+
+});
+
+app.post('/', mdAutenticacion.verificarToken, (req,res)=>{
+    var body = req.body;
+
+    var metas = new Meta({
+        year: body.year,
+        metas: body.metas
+    });
+
+    metas.save((err, metasGuardadas)=>{
+
+        if(err){
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al crear meta',
+                errors: err
+            });
+        }
+
+        res.status(201).json({
+            ok: true,
+            mensaje: 'Meta creada exitosamente',
+            metaCreada: metasGuardadas
+        });
+    }); 
+
+});
+
+app.put('/:id', mdAutenticacion.verificarToken, (req,res)=>{
+    var id=req.params.id;
+    var body=req.body;
+
+    Meta.findById(id)
+     .exec((err, meta)=>{
+
+        if(err){
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar meta',
+                errors: err
+            });
+        }
+
+        if(!meta){
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'La meta id: ' + id + ', no existe en la base de datos',
+                errors: { message: 'La meta que se desea actualizar no existe en la base de datos' }
+            });
+        }
+
+        meta.metas=body.metas;
+
+        meta.save((err,metaActualizada)=>{
+
+            if(err){
+                return res.status(500).json({
+                    ok:false,
+                    mensaje: 'Error al actualizar meta',
+                    errors: err
+                });
+            }
+
+            res.status(200).json({
+                ok: true,
+                mensaje: 'Meta actualizada exitosamente',
+                meta: metaActualizada
+            });
+
+        });
+    });
+});
+
+module.exports = app;
