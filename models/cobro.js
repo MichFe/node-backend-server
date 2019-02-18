114f5b08e1b25d338c6958021bfa90d5bf089afd
Michelle Felix <mich.feloz@gmail.com>
2019-02-18 04:12:33 -0600

    Modelo de cobro

---
+++ models/cobro.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var cobroSchema = new Schema({
+    venta: { type: Schema.Types.ObjectId, ref: 'Venta', required: [true, 'Es necesario asociar el pago a una venta'] },
+    cliente: { type: Schema.Types.ObjectId, ref: 'Usuario', reqired: [true, 'Es necesario asociar el pago a un cliente'] },
+    monto: { type: Number, required: true },
+    tipoDePago: { type: String, required: true },
+    fecha: { type: Date, required: true }
+});
+
+module.exports = mongoose.model('Cobro', cobroSchema); 
