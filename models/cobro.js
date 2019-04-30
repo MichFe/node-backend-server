81fce6af71cd704f3b8b20f4906a266ad737d009
Michelle Felix <mich.feloz@gmail.com>
2019-04-30 15:04:06 -0500

    Collection reference correcter

--- models/cobro.js
+++ models/cobro.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 
 var cobroSchema = new Schema({
     venta: { type: Schema.Types.ObjectId, ref: 'Venta', required: [true, 'Es necesario asociar el pago a una venta'] },
-    cliente: { type: Schema.Types.ObjectId, ref: 'Usuario', required: [true, 'Es necesario asociar el pago a un cliente'] },
+    cliente: { type: Schema.Types.ObjectId, ref: 'Cliente', required: [true, 'Es necesario asociar el pago a un cliente'] },
     monto: { type: Number, required: true },
     tipoDePago: { type: String, required: true },
     fecha: { type: Date, required: true }
