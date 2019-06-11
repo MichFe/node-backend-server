33c992f77019b88037422a8e9b96a0edcc1fd7a0
Michelle Felix <mich.feloz@gmail.com>
2019-06-11 18:12:28 -0500

    Actualizacion de menu default

--- data/menuDefault.js
+++ data/menuDefault.js
@@ -1,63 +1,81 @@
 module.exports.MENU_DEFAULT = [
-    {
-        modulo: "Vender",
-        icono: "mdi mdi-cart",
+  {
+    modulo: "Vender",
+    icono: "mdi mdi-cart",
+    url: "/catalogoProductos",
+    show: false
+  },
+  {
+    modulo: "Ventas",
+    icono: "mdi mdi-cash-usd",
+    url: "/reporteVentas",
+    show: false,
+    submenu: [
+      { titulo: "CRM", url: "/crm", show: false },
+      { titulo: "Reporte de Ventas", url: "/reporteVentas", show: false },
+      {
+        titulo: "Catálogo de Productos",
         url: "/catalogoProductos",
         show: false
-    },
-    {
-        modulo: "Ventas",
-        icono: "mdi mdi-cash-usd",
-        url: "/reporteVentas",
-        show: false,
-        submenu: [
-            { titulo: "CRM", url: "/crm", show: false },
-            { titulo: "Reporte de Ventas", url: "/reporteVentas", show: false },
-            { titulo: "Catálogo de Productos", url: "/catalogoProductos", show: false },
-            { titulo: "Cuentas por cobrar", url: "/cuentasPorCobrar", show: false }
-        ]
-    },
-    {
-        modulo: "Compras",
-        icono: "mdi mdi-tag-text-outline",
-        url: "/requisiciones",
-        show: false,
-        submenu: [
-            { titulo: "Requisiciones", url: "/requisiciones", show: false },
-            { titulo: "Aprobaciones", url: "/aprobaciones", show: false },
-            { titulo: "Compras", url: "/compras", show: false },
-            { titulo: "Cuentas por pagar", url: "/cuentasPorPagar", show: false }
-        ]
-    },
-    {
-        modulo: "Gastos",
-        icono: "ti-wallet",
-        url: "/gastos",
-        show: false,
-        submenu: [
-            { titulo: "Registro de gastos", url: "/gastos", show: false },
-            { titulo: "Reporte de gastos", url: "/reporteDeGastos", show: false }
-        ]
-    },
-    {
-        modulo: "Equipo",
-        icono: "mdi mdi-account-multiple",
-        url: "/nomina",
-        show: false,
-        submenu: [
-            { titulo: "Nomina", url: "/nomina", show: false }
-        ]
-    },
-    {
-        modulo: "Configuración",
-        icono: "mdi mdi-settings",
-        url: "/permisosDeUsuario",
-        show: false,
-        submenu: [
-            { titulo: "Permisos", url: "/permisosDeUsuario", show: false },
-            { titulo: "Configuración de menú", url: "/configuracionDeMenu", show: false }
-        ]
-    }
+      },
+      { titulo: "Cuentas por cobrar", url: "/cuentasPorCobrar", show: false }
+    ]
+  },
+  {
+    modulo: "Compras",
+    icono: "mdi mdi-tag-text-outline",
+    url: "/requisiciones",
+    show: false,
+    submenu: [
+      { titulo: "Requisiciones", url: "/requisiciones", show: false },
+      { titulo: "Aprobaciones", url: "/aprobaciones", show: false },
+      { titulo: "Compras", url: "/compras", show: false },
+      { titulo: "Cuentas por pagar", url: "/cuentasPorPagar", show: false }
+    ]
+  },
+  {
+    modulo: "Gastos",
+    icono: "ti-wallet",
+    url: "/gastos",
+    show: false,
+    submenu: [
+      { titulo: "Registro de gastos", url: "/gastos", show: false },
+      { titulo: "Reporte de gastos", url: "/reporteDeGastos", show: false }
+    ]
+  },
+  {
+    modulo: "Inventario",
+    icono: "mdi mdi-package-variant-closed",
+    url: "/inventarioTienda",
+    show: false,
+    submenu: [
+      { titulo: "Inventario tienda", url: "/inventarioTienda", show: false }
+    ]
+  },
+  {
+    modulo: "Equipo",
+    icono: "mdi mdi-account-multiple",
+    url: "/nomina",
+    show: false,
+    submenu: [
+      { titulo: "Nomina", url: "/nomina", show: false },
+      { titulo: "Usuarios", url: "/usuarios", show: false }
+    ]
+  },
+  {
+    modulo: "Configuración",
+    icono: "mdi mdi-settings",
+    url: "/permisosDeUsuario",
+    show: false,
+    submenu: [
+      { titulo: "Permisos", url: "/permisosDeUsuario", show: false },
+      {
+        titulo: "Configuración de menú",
+        url: "/configuracionDeMenu",
+        show: false
+      }
+    ]
+  }
 ]; 
 
 module.exports.MENU_DEFAULT_ADMIN = [
@@ -102,12 +120,23 @@ module.exports.MENU_DEFAULT_ADMIN = [
         ]
     },
     {
+        modulo: "Inventario",
+        icono: "mdi mdi-package-variant-closed",
+        url: "/inventarioTienda",
+        show: true,
+        submenu: [
+            { titulo: "Inventario tienda", url: "/inventarioTienda", show: true }
+        ]
+
+    },
+    {
         modulo: "Equipo",
         icono: "mdi mdi-account-multiple",
         url: "/nomina",
         show: true,
         submenu: [
-            { titulo: "Nomina", url: "/nomina", show: true }
+            { titulo: "Nomina", url: "/nomina", show: true },
+            { titulo: "Usuarios", url: "/usuarios", show: true }
         ]
     },
     {
